7818978b47db6f1a91cf1ebf23e459af21e8b7fb
krishnatejak <krishnatejakannaiah@gmail.com>
2023-07-15 04:14:44 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const errorHandler = require("./middleware/errorhandler");
 const connectDb = require("./config/dbconnection");
+const { default: mongoose } = require("mongoose");
 const dotenv = require('dotenv').config();
 
 connectDb();
@@ -12,6 +13,12 @@ const port = process.env.PORT || 5005;
 app.use(express.json());
 app.use("/api/contacts", require('./routes/contactroute'));
 app.use(errorHandler);
+
+mongoose.set('strictQuery', false);
+
+app.get('/', (req,res) => {
+    res.send({title: 'RedRingFires'})
+})
 
 app.listen(port, () => {
     console.log(`server is running on  ${port}`);
